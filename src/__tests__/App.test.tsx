7e043a4dd7edc29565d0bfecb077fe8a653b3d4f
Hartaithan <hartaithan@gmail.com>
2023-09-04 22:26:11 +0900

    setup jest

---
+++ src/__tests__/App.test.tsx
@@ -0,0 +1,11 @@
+import "@testing-library/jest-dom";
+import { render } from "@testing-library/react";
+import App from "../App";
+
+describe("App", () => {
+  test("renders without errors", () => {
+    expect(() => {
+      render(<App />);
+    }).not.toThrow();
+  });
+});
