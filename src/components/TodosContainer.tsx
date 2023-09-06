fccd87ca2a502ffb3e3d68ccd0394ea8c5be85dc
Hartaithan <hartaithan@gmail.com>
2023-09-06 21:55:57 +0900

    style todos container

--- src/components/TodosContainer.tsx
+++ src/components/TodosContainer.tsx
@@ -1,15 +1,43 @@
 import type { FC } from "react";
-import { styled } from "styled-components";
+import { css, styled } from "styled-components";
 import TodoInput from "./TodoInput";
 import TodosList from "./TodosList";
 import TodosFilters from "./TodosFilters";
 
+const shadow = css`
+  box-shadow: 0px 3px 4px 0px rgba(0, 0, 0, 0.2);
+  -moz-box-shadow: 0px 3px 4px 0px rgba(0, 0, 0, 0.2);
+  -webkit-box-shadow: 0px 3px 4px 0px rgba(0, 0, 0, 0.2);
+`;
+
+const card = css`
+  content: "";
+  position: absolute;
+  height: 100%;
+  left: 50%;
+  background: #ffffff;
+  transform: translateX(-50%);
+`;
+
 const Container = styled.div`
+  position: relative;
   background: #ffffff;
   width: 500px;
-  box-shadow: 0px 3px 6px 0px rgba(0, 0, 0, 0.2);
-  -moz-box-shadow: 0px 3px 6px 0px rgba(0, 0, 0, 0.2);
-  -webkit-box-shadow: 0px 3px 6px 0px rgba(0, 0, 0, 0.2);
+  ${shadow}
+  &:after {
+    z-index: -1;
+    bottom: -5px;
+    width: 98%;
+    ${shadow}
+    ${card}
+  }
+  &:before {
+    z-index: -2;
+    bottom: -10px;
+    width: 96%;
+    ${shadow}
+    ${card}
+  }
 `;
 
 const TodosContainer: FC = () => {
